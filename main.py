0f2d34c078d4904fce7542ca79dfc240f30d11e1
Andreas 'count' Kotes <count@c-base.org>
2013-10-01 23:40:25 +0200

    split up for better testing

--- main.py
+++ main.py
@@ -16,10 +16,18 @@ import api
 from api.database import Database
 
 import sys
-if __name__ == '__main__':
+
+config = {}
+sslcontext = None
+app = None
+
+def load_config():
+  global config
   configfile = os.getenv('CTEWARD_ST_LEXWARE_CONFIG', '/etc/cteward/st-lexware.json')
   config = json.load(open(configfile))
 
+def check_config():
+  global config
   # runner not configured? -> default to 127.0.0.1:14338
   if not 'runner' in config:
     config['runner'] = {
@@ -40,20 +48,37 @@ if __name__ == '__main__':
       }
     }
 
+def setup_ssl():
+  global sslcontext
   # SSL configured? set up context for server
-  if not 'ssl' in config:
-    sslcontext = None
-  else:
+  if 'ssl' in config:
     sslcontext = SSL.Context(SSL.SSLv23_METHOD)
     sslcontext.use_certificate_file(config['ssl']['cert'])
     sslcontext.use_privatekey_file(config['ssl']['key'])
 
+def setup_database():
   # database configured? initialize it!
   if 'database' in config:
     Database.init(config['database'])
 
+def setup_flask():
+  global app
   # set up Flask object, configure it, add API, and go!
   app = Flask('st-lexware')
   app.config.update(**config['flask'])
+
+def setup_api():
   api.init(app, config=config['api'])
+
+def run_app():
+  global app
   app.run(ssl_context=sslcontext, **config['runner'])
+
+if __name__ == '__main__':
+  load_config()
+  check_config()
+  setup_ssl()
+  setup_database()
+  setup_flask()
+  setup_api()
+  run_app()
