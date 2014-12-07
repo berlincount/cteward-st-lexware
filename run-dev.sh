524b9ac4b79118b668ab7e315d54d7478242d131
Andreas 'count' Kotes <count@flatline.de>
2014-12-07 21:27:24 +0100

    add basic request logging

--- run-dev.sh
+++ run-dev.sh
@@ -2,5 +2,5 @@
 
 cd `/usr/bin/dirname $0`
 export CTEWARD_ST_LEXWARE_CONFIG=st-lexware-dev.json
-/usr/local/bin/nodemon -x nodejs app.js | /usr/bin/nodejs node_modules/.bin/bunyan 2>&1
+/usr/local/bin/nodemon -x nodejs app.js | /usr/bin/nodejs node_modules/.bin/bunyan -o short 2>&1
 
